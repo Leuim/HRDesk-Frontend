0cfd511ff1c9cb4f4a4c2e093d84ab559c2d7bc0
leuim <jassim.mohammed48@gmail.com>
2025-08-05 11:46:22 +0300

    done the sign in/ sign out features

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,15 +2,30 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router'
 import { UserContext } from '../../contexts/UserContext'
 const NavBar = () => {
-  const { user } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
+
+  const handleSignout = () => {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
   return (
     <nav>
-      <ul>{user ? (
-        <li><Link to='/sign-up'>Sign Up</Link></li>) :
-        (<>
-          <li><Link>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
-        </>)}
+      <ul>
+        {user ? (
+          <>
+            <li><Link to='/' onClick={handleSignout}>Sign Out</Link></li>
+            {user.role === 'admin' ? (
+              <li><Link to='/admin-dashboard'>Admin dashboard</Link></li>
+            ) : (
+              <li><Link to='/employee-dashboard'>Employee dashboard</Link></li>
+            )}
+          </>
+        ) : (
+          <>
+            <li><Link to='/sign-in'>Sign In</Link></li>
+            <li><Link to='/sign-up'>Sign Up</Link></li>
+          </>
+        )}
       </ul>
     </nav>
   )
