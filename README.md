f3daabcbf5ec52573483b059bb561c61639f273e
leuim <jassim.mohammed48@gmail.com>
2025-08-15 19:20:00 +0300

    completed the readme now completing the deployement setup

---
+++ README.md
@@ -0,0 +1,37 @@
+![Website landing page](https://i.imgur.com/8dbgOg2.png)
+# HRDesk
+
+HRDesk is a web application which allows normal employees to submit leave requests and easily view their leave balance in a simple way. In addition, it enables HR to easliy manage leave requests and either approve or reject submitted leave request and to also manage employees records while also beign able to view other reviewed leave requests.
+
+
+
+
+## Getting Started
+> # [Live Demo]()
+### Project Planning
+### [Link To Trello Board](https://trello.com/b/9THMnS2M/hrdesk)
+### [Link To BackEnd Repo](https://github.com/Leuim/HRDesk-Backend)
+### 1. ERD
+
+>![HRDesk ERD](https://i.imgur.com/4KLd4Mb.png)
+
+### 2. WireFrames
+
+>![HRDesk WireFrames](https://i.imgur.com/uldCxKY.png)
+
+## Attribution 
+list of tools/libraries and assests used in the project
+1. bootstrap
+2. [For the Logo](https://www.design.com/ai-logo-generator)
+
+## Technologies Used
+1. MongoDB
+2. Node.js
+3. Express.js
+4. Vite + React
+
+## Future Goals
+1. Allow for the admins to create and add new leave types instead of them being hardcoded
+2. Add a calander for a refrence when submitting leave request
+3. Add multiple HR usecases like salary deduction/ shift planning 
+4. Leave request history export as pdf or excel
--- src/components/EmployeeDashboard/EmployeeDashboard.jsx
+++ src/components/EmployeeDashboard/EmployeeDashboard.jsx
@@ -24,7 +24,6 @@ useEffect(() => {
       
       if (isMounted) {
         setBalance(data);
-        console.log('Updated balance:', data); 
       }
     } catch (err) {
       if (isMounted) {
@@ -58,22 +57,22 @@ useEffect(() => {
     
       <h1>Hello  {user.name}, Welcome to Employee dashboard</h1>
 
-    <div class="container text-center py-5" >
+    <div className="container text-center py-5" >
 
-  <h1 class='dashboard-title'>  Leave Balance</h1>
+  <h1 className='dashboard-title'>  Leave Balance</h1>
       {balance && (
         <>
         
-        <div class='dashboard-card'> 
+        <div className='dashboard-card'> 
                 
-         <div class='card-1 '> <p>Annual: </p>  {balance.annual} days</div>
-          <div class='card-2'><p>Sick: </p>{balance.sick} days</div>
-         <div class='card-3'><p>Other:</p>{balance.others} days</div>
+         <div className='card-1 '> <p>Annual: </p>  {balance.annual} days</div>
+          <div className='card-2'><p>Sick: </p>{balance.sick} days</div>
+         <div className='card-3'><p>Other:</p>{balance.others} days</div>
         </div>
         </>
       )}
 
-       <div class='dashboard-btn'> 
+       <div className='dashboard-btn'> 
          <button  className='btn-new-leave'> <Link to="/leaveRequest" >
           Request Leave
         </Link> </button>
